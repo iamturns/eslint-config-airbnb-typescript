464b0850e09cbe218999bd986f5656d30e868c96
Matt Turnbull <matt@iamturns.com>
2019-01-27 14:35:55 +1100

    feat: only apply config to ts files (#2)

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 module.exports = {
   extends: ['eslint-config-airbnb', './lib/shared'].map(require.resolve),
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      settings: {
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+          },
+        },
+        'import/extensions': ['.js', '.ts', '.mjs', '.jsx', '.tsx'],
+      },
+      rules: {
+        // only .jsx and .tsx files may have JSX
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+        'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
       },
     },
-    'import/extensions': ['.js', '.ts', '.mjs', '.jsx', '.tsx'],
-  },
-  rules: {
-    // only .jsx and .tsx files may have JSX
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
-    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
-  },
+  ],
 };
