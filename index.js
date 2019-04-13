3b4e857cac2f20692e86847d836ba063c3b645b2
Matt Turnbull <matt@iamturns.com>
2019-04-13 10:58:42 +1000

    fix: disable eslint rules that are incompatible with typescript
    These rules are now replaced with equivalent 'typescript-eslint' rules.
    
    BREAKING CHANGE: Config may find additional linting errors for: `camelcase`, `indent`, `no-array-constructor`, and `no-unused-vars`.

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+// This file adds some React specific settings. Not using React? Use base.js instead.
 module.exports = {
   extends: ['eslint-config-airbnb', './lib/shared'].map(require.resolve),
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       settings: {
+        // Append 'ts' and 'tsx' extensions to Airbnb 'import/resolver' setting
         'import/resolver': {
           node: {
             extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
           },
         },
+        // Append 'ts' and 'tsx' extensions to Airbnb 'import/extensions' setting
         'import/extensions': ['.js', '.ts', '.mjs', '.jsx', '.tsx'],
       },
       rules: {
-        // only .jsx and .tsx files may have JSX
+        // Append 'tsx' to Airbnb 'react/jsx-filename-extension' rule
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
         'react/jsx-filename-extension': [
           'error',
