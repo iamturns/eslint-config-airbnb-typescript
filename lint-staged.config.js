bbbb572ddfb7fc067cbf211288e6757140d4cc10
Matt Turnbull <matt@iamturns.com>
2019-11-14 21:30:13 +1100

    chore: Update Prettier settings (#45)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "**/*.{json,md,yml}": ["prettier --write", "git add"],
-  "**/*.js": ["prettier --write", "eslint --cache --fix", "git add"],
-  ".editorconfig": ["prettier --write", "git add"],
-  LICENSE: ["prettier --write", "git add"],
-  "package.json": ["prettier --write", "npm run format:package", "git add"],
-}
+  '**/*.{json,md,yml}': ['prettier --write', 'git add'],
+  '**/*.js': ['prettier --write', 'eslint --cache --fix', 'git add'],
+  '.editorconfig': ['prettier --write', 'git add'],
+  LICENSE: ['prettier --write', 'git add'],
+  'package.json': ['prettier --write', 'npm run format:package', 'git add'],
+};
--- prettier.config.js
+++ prettier.config.js
@@ -1,18 +1,25 @@
 // Some settings automatically inherited from .editorconfig
 
 module.exports = {
-  // Why include an unnecessary character at the end of every line? Break the habit (automatically)!
-  semi: false,
-  // Trailing commas help with git merging and conflict resolution
-  trailingComma: "all",
+  printWidth: 100, // https://github.com/airbnb/javascript#19.13
+  tabWidth: 2, // https://github.com/airbnb/javascript#19.1
+  useTabs: false, // https://github.com/airbnb/javascript#19.1
+  semi: true, // https://github.com/airbnb/javascript#21.1
+  singleQuote: true, // https://github.com/airbnb/javascript#6.1
+  quoteProps: 'as-needed', // https://github.com/airbnb/javascript#3.6
+  jsxSingleQuote: false, // https://github.com/airbnb/javascript/tree/master/react#quotes
+  trailingComma: 'all', // https://github.com/airbnb/javascript#20.2
+  bracketSpacing: true, // https://github.com/airbnb/javascript#19.12
+  jsxBracketSameLine: false, // https://github.com/airbnb/javascript/tree/master/react#alignment
+  arrowParens: 'always', // https://github.com/airbnb/javascript#8.4
   overrides: [
     {
-      files: ".editorconfig",
-      options: { parser: "yaml" },
+      files: '.editorconfig',
+      options: { parser: 'yaml' },
     },
     {
-      files: "LICENSE",
-      options: { parser: "markdown" },
+      files: 'LICENSE',
+      options: { parser: 'markdown' },
     },
   ],
-}
+};
