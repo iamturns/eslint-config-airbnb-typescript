11893430c4d0d259e01c45e50426e61bf4515819
Kenneth Sills <132029135+Kenneth-Sills@users.noreply.github.com>
2024-08-05 00:08:27 +0000

    build: changes required for dogfooding

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
+const stylisticPlugin = require('@stylistic/eslint-plugin');
+
 module.exports = {
   extends: ['eslint-config-airbnb-base', './base.js', 'prettier'],
   parserOptions: {
     project: './tsconfig.json',
   },
+  rules: Object.fromEntries(
+    Object.keys(stylisticPlugin.configs['all-flat'].rules ?? {}).map((key) => [key, 'off']),
+  ),
 };
